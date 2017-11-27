e49c1aa336b2ac8cb3751dfb1568baddac4bdc4e
Mike McNeil <git+mikermcneil@sailsjs.com>
2017-11-27 16:09:31 -0600

    Consolidate hooks

--- README.md
+++ README.md
@@ -1,10 +1,22 @@
 [![Redirecting http requests to https in Node.js with Sails.js](https://camo.githubusercontent.com/9e49073459ed4e0e2687b80eaf515d87b0da4a6b/687474703a2f2f62616c64657264617368792e6769746875622e696f2f7361696c732f696d616765732f6c6f676f2e706e67)](https://sailsjs.com)
 
-# sails-hook-lifejacket
+# lifejacket
 
-Redirect http:// to https:// in your Node.js/Sails.js application to ensure TLS/SSL.
+Redirect http:// to https:// in your Node.js/Sails.js application to ensure TLS/SSL, plus a bit of middleware for handling LetsEncrypt cert renewals.
 
-> Be sure to [enable secure cookies](https://sailsjs.com/documentation/reference/configuration/sails-config-http)!  Otherwise, the initial http:// request (before redirecting) could still transmit sensitive data in plain text.
+> When using SSL, also be sure to always [enable secure cookies](https://sailsjs.com/documentation/reference/configuration/sails-config-http)!  Otherwise, the initial http:// request (before redirecting) could still transmit sensitive data in plain text.
+
+
+-----------------------------------
+
+#### Disclaimer
+
+If you can get away with it, I'd always recommend buying a wildcard SSL cert and then using a tool like Cloudflare to handle SSL redirects automatically.**  The convenience and lack of _yet another thing to maintain_ makes the addition of another layer of infrastructure _well worth it_, IMO.  (Here's the [cheat sheet](https://gist.github.com/mikermcneil/d3114517e4c7263b145274ea4ad3d0cb) I use when setting up Cloudflare with Heroku.)
+
+That said, this hook exists for those situations where buying a wildcard cert (~$100-150 per year) or setting up a free Cloudflare account is not an option, for whatever reason.
+
+-----------------------------------
+
 
 
 ## Installation
@@ -12,7 +24,7 @@ Redirect http:// to https:// in your Node.js/Sails.js application to ensure TLS/
 To install this hook in your Sails app, just run:
 
 ```bash
-$ npm install sails-hook-lifejacket --save
+$ npm install lifejacket --save
 ```
 
 Then set the following configuration (probably in your config/env/production.js file):
@@ -40,14 +52,14 @@ Then, when you lift in production, `http://` requests should redirect to `https:
 See [Extending Sails > Hooks](https://sailsjs.com/documentation/concepts/extending-sails/hooks) in the [Sails documentation](https://sailsjs.com/documentation), or check out [recommended support options](https://sailsjs.com/support).
 
 
-## Contributing &nbsp; [![Build Status](https://travis-ci.org/sailshq/sails-hook-lifejacket.svg?branch=master)](https://travis-ci.org/sailshq/sails-hook-lifejacket) &nbsp; [![Build status on Windows](https://ci.appveyor.com/api/projects/status/u0i1o62tsw6ymbjd/branch/master?svg=true)](https://ci.appveyor.com/project/mikermcneil/sails-hook-lifejacket/branch/master)
+## Contributing &nbsp; [![Build Status](https://travis-ci.org/sailshq/lifejacket.svg?branch=master)](https://travis-ci.org/sailshq/lifejacket) &nbsp; [![Build status on Windows](https://ci.appveyor.com/api/projects/status/u0i1o62tsw6ymbjd/branch/master?svg=true)](https://ci.appveyor.com/project/mikermcneil/lifejacket/branch/master)
 
 Please observe the guidelines and conventions laid out in the [Sails project contribution guide](https://sailsjs.com/documentation/contributing) when opening issues or submitting pull requests.
 
-[![NPM](https://nodei.co/npm/sails-hook-lifejacket.png?downloads=true)](http://npmjs.com/package/sails-hook-lifejacket)
+[![NPM](https://nodei.co/npm/lifejacket.png?downloads=true)](http://npmjs.com/package/lifejacket)
 
 
-## Bugs &nbsp; [![NPM version](https://badge.fury.io/js/sails-hook-lifejacket.svg)](http://npmjs.com/package/sails-hook-lifejacket)
+## Bugs &nbsp; [![NPM version](https://badge.fury.io/js/lifejacket.svg)](http://npmjs.com/package/lifejacket)
 
 To report a bug, [click here](https://sailsjs.com/bugs).
 
@@ -59,6 +71,6 @@ This [community hook](https://sailsjs.com/documentation/concepts/extending-sails
 
 As for the [Sails framework](https://sailsjs.com)?  It's free and open-source under the [MIT License](https://sailsjs.com/license).
 
-&copy; [The Sails Company](https://sailsjs.com/about)
+&copy; [Mike McNeil](https://sailsjs.com/about)
 
 ![image_squidhome@2x.png](http://i.imgur.com/RIvu9.png)
--- index.js
+++ index.js
@@ -8,28 +8,59 @@ var flaverr = require('flaverr');
 
 
 /**
- * lifejacket hook
+ * lifejacket
  *
- * @description :: A hook definition.  Extends Sails by adding shadow routes, implicit actions, and/or initialization logic.
- * @docs        :: http://sailsjs.com/docs/concepts/extending-sails/hooks
+ * A Sails hook for taking care of stuff related to SSL.
+ * (BUT SEE DISCLAIMER IN README!  THERE ARE BETTER WAYS TO DO THIS!)
+ *
+ * Handle LE/ACME challenges and do `http://`->`https://` redirects.
+ *
+ * • About LetsEncrypt (LE):
+ *   —· https://letsencrypt.org/
+ *
+ * • About Automatic Certificate Management Environment (ACME):
+ *   —· https://github.com/ietf-wg-acme/acme
+ *
+ * > Inspired by @simonratner's koa-acme (https://github.com/simonratner/koa-acme)
+ * >
+ * > MIT License
+ * > Copyright 2016 Simon Ratner, 2017 Mike McNeil.
  */
 
-module.exports = function defineLifejacketHook(sails) {
+module.exports = function lifejacket(sails) {
+
+  // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+  // NOTE: It is not clear if we'll need to use this or not-- it'll depend
+  // a lot on our production setup (and e.g. whether `req.secure` / `req.host`
+  // are actually meaningful/correct on our production server.)
+  // For now, it is disabled here by default.
+  //
+  // (See also https://github.com/themadcreator/acme-express for another way to do this)
+  //
+  // Also, note that with Heroku, as of March 27, 2017, this stuff is now
+  // (more or less) taken care of automatically:
+  // -· https://devcenter.heroku.com/articles/automated-certificate-management
+  // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
   return {
-
 
     defaults: {
 
       lifejacket: {
 
+        // Auto-redirect HTTP to HTTPS?
         // Disabled by default. (e.g. for local dev)
         ensureHttps: false,
 
-        // Must be set manually if `ensureHttps` is enabled.
+        // Must be set manually if `ensureHttps` is enabled-- or better yet,
+        // use sails.config.custom.baseUrl instead.  (This looks there if not
+        // specified.)
         // > Should be provided as a string, like `foo.example.com`.
-        host: undefined
+        host: undefined,
 
+        // ACME challenge credentials for LetsEncrypt
+        acmeChallengeToken: undefined,
+        acmeKeyAuthorization: undefined
       }
 
     },
@@ -37,27 +68,31 @@ module.exports = function defineLifejacketHook(sails) {
 
     configure: function (){
 
-      // If `ensureHttps` config was provided...
-      if (!_.isUndefined(sails.config.lifejacket.ensureHttps)) {
-
-        // Validate it.
+      if (sails.config.lifejacket.ensureHttps !== undefined) {
         if (!_.isBoolean(sails.config.lifejacket.ensureHttps)) {
-          throw flaverr({ name: 'ConfigurationError' }, new Error('If provided, `sails.config.lifejacket.ensureHttps` must be set to either `true` or `false`.  But instead got: '+util.inspect(sails.config.lifejacket.ensureHttps, {depth:null})));
+          throw flaverr({ name: 'Configuration Error' }, new Error('If provided, `sails.config.lifejacket.ensureHttps` must be set to either `true` or `false`.  But instead got: '+util.inspect(sails.config.lifejacket.ensureHttps, {depth:null})));
         }
+      }//ﬁ
 
-      }//>-•
+      if (sails.config.letsEncrypt.acmeChallengeToken !== undefined && !_.isString(sails.config.letsEncrypt.acmeChallengeToken)) {
+        throw new Error('Configuration error in LetsEncrypt hook: If set, "letsEncrypt.acmeChallengeToken" must be set as a valid string.  Instead, got: '+util.inspect(sails.config.letsEncrypt.acmeChallengeToken));
+      }
+      if (sails.config.letsEncrypt.acmeKeyAuthorization !== undefined && !_.isString(sails.config.letsEncrypt.acmeKeyAuthorization)) {
+        throw new Error('Configuration error in LetsEncrypt hook: If set, "letsEncrypt.acmeKeyAuthorization" must be set as a valid string.  Instead, got: '+util.inspect(sails.config.letsEncrypt.acmeKeyAuthorization));
+      }
 
-
-      // If `host` config was provided...
+      // If `host` config was provided, or if falling back to sails.config.custom.baseUrl yields something...
+      sails.config.lifejacket.host = sails.config.lifejacket.host || sails.config.custom.baseUrl || undefined;
       if (!_.isUndefined(sails.config.lifejacket.host)) {
 
         // Validate it.
         if (!_.isString(sails.config.lifejacket.host)) {
-          throw flaverr({ name: 'ConfigurationError' }, new Error('`sails.config.lifejacket.host` must be configured as a string (like `foo.example.com`).  But instead got: '+util.inspect(sails.config.lifejacket.host, {depth:null})));
+          throw flaverr({ name: 'Configuration Error' }, new Error('`sails.config.lifejacket.host` must be configured as a string (like `foo.example.com`).  But instead got: '+util.inspect(sails.config.lifejacket.host, {depth:null})));
         }
 
-        // Coerce it.
+        // Coerce it to strip off path/querystring/fragment/port/protocol
         sails.config.lifejacket.host = _.trimRight(sails.config.lifejacket.host, '/');
+        sails.config.lifejacket.host = sails.config.lifejacket.host.replace(/\:[0-9]+$/, '');
         sails.config.lifejacket.host = sails.config.lifejacket.host.replace(/^https?:\/\//, '');
 
       }
@@ -66,7 +101,7 @@ module.exports = function defineLifejacketHook(sails) {
       else {
 
         if (sails.config.lifejacket.ensureHttps) {
-          throw flaverr({ name: 'ConfigurationError' }, new Error('Since `sails.config.lifejacket.ensureHttps` is enabled, a valid `sails.config.lifejacket.host` must be configured as a string (like `foo.example.com`).  But instead got: '+util.inspect(sails.config.lifejacket.host, {depth:null})));
+          throw flaverr({ name: 'Configuration Error' }, new Error('Since `sails.config.lifejacket.ensureHttps` is enabled, a valid `sails.config.lifejacket.host` must be configured as a string (like `staging.example.com`).  But instead got: '+util.inspect(sails.config.lifejacket.host, {depth:null})));
         }
 
       }//>-•
@@ -104,6 +139,31 @@ module.exports = function defineLifejacketHook(sails) {
 
     routes: {
       before: {
+
+        // Respond to acme http-01 challenge requests.
+        '/.well-known/acme-challenge/*': {
+          skipAssets: true,
+          fn: function (req, res, next) {
+
+            // If challenge token or authorization are missing, then fail silently.
+            if (!sails.config.letsEncrypt.acmeChallengeToken || !sails.config.letsEncrypt.acmeKeyAuthorization) {
+              return next();
+            }//•
+
+            // Only respond if the supposed challenge token is valid.
+            var supposedChallengeToken = req.param('0');
+            var actualChallengeToken = sails.config.letsEncrypt.acmeChallengeToken;
+            if (supposedChallengeToken !== actualChallengeToken) {
+              return next();
+            }//•
+
+            var keyAuthorization = sails.config.letsEncrypt.acmeKeyAuthorization;
+            return res.send(keyAuthorization);
+
+          }
+        },
+
+        // Redirect http:// to https:// if configured to do so.
         'all /*': function (req, res, next) {
 
           // console.log('x-forwarded-proto:', req.get('x-forwarded-proto'));
@@ -112,33 +172,40 @@ module.exports = function defineLifejacketHook(sails) {
           // If the `lifejacket.ensureHttps` setting is disabled, then anything goes.
           if (!sails.config.lifejacket.ensureHttps) {
             return next();
-          }//-•
+          }//•
 
           // If is a virtual request from a socket, then anything goes.
+          // (Don't try to redirect)
           if (req.isSocket) {
             return next();
-          }//-•
+          }//•
+
 
           // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-          // But otherwise, this is an HTTP request of some kind, and we must be
-          // interested in redirecting it, since we're configured to do so.
+          // This must be an HTTP request of some kind, and we must be interested
+          // in redirecting it, since we're configured to do so.
           //
           // Therefore, we'll check this request and see if it was originally sent
           // via the `http://` protocol.  If so, we'll redirect it to `https://`.
           // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-
-          // If this is https://, then we're good.
-          // (we'll go ahead and allow the request through)
-          if (req.get('x-forwarded-proto') === 'https') {
+          var isHttpsRequest = req.get('x-forwarded-proto') === 'https' || (req.host && req.protocol==='https');
+          if (isHttpsRequest) {
+            // If this is https://, then we're good.
+            // (we'll go ahead and allow the request through)
             return next();
-          }//-•
-
-          // Otherwise, this is an insecure request to `http://`.
-          // So redirect it to its `https://` cousin.
-          return res.redirect(301, 'https://'+sails.config.lifejacket.host+req.url);
+            // Always allow unsecure (http://) requests to the ACME challenge
+            // request endpoint.
+          } else if (req.url.match(/^\/\.well-known\/acme-challenge\/(.+)/)) {
+            return next();
+          } else {
+            // But otherwise, redirect to the https:// equivalent.
+            // Otherwise, this is an insecure request to `http://`.
+            // So redirect it to its `https://` cousin.
+            return res.redirect(301, 'https://'+sails.config.lifejacket.host+req.url);
+          }
 
         }//</ all /* >
+
       }//</.before>
     }//</.routes>
 
